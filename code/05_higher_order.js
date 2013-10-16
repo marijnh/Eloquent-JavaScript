901d2778fefd277d7e9ae8edc308079f3861fdb4
Marijn Haverbeke <marijnh@gmail.com>
2013-10-16 11:51:18 +0200

    Second pass over chapter 5

--- code/05_higher_order.js
+++ code/05_higher_order.js
@@ -14,3 +14,13 @@ function reduceAncestors(person, f, zero) {
   }
   return handle(person, 0);
 }
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});

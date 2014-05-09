1ef2130a05b9893a0deb77d7e2f5cbeb6f6fbd88
Marijn Haverbeke <marijnh@gmail.com>
2014-05-09 13:36:32 +0200

    Integrate editing for chapter 6

--- code/mountains.js
+++ code/mountains.js
@@ -4,6 +4,6 @@ var MOUNTAINS = [
   {name: "Mount Fuji", height: 3776, country: "Japan"},
   {name: "Mont Blanc", height: 4808, country: "Italy/France"},
   {name: "Vaalserberg", height: 323, country: "Netherlands"},
-  {name: "Mount McKinley", height: 6168, country: "United States"},
+  {name: "Denali", height: 6168, country: "United States"},
   {name: "Popocatepetl", height: 5465, country: "Mexico"}
 ];

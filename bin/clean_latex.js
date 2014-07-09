d6a6a6d774964c5eb8b97e0743b5bffb69cf43cc
Marijn Haverbeke <marijnh@gmail.com>
2014-07-09 16:04:22 +0200

    Fix workaround for stray underscore in tex output

--- bin/clean_latex.js
+++ bin/clean_latex.js
@@ -3,11 +3,13 @@ process.stdin.on("data", function(chunk) {
   input += chunk;
 });
 process.stdin.on("end", function() {
-  process.stdout.write(input.replace(/(\n\n\\end{Code})|(\n{3,})/g, function(all, codeSpace, manyBlanks) {
+  process.stdout.write(input.replace(/(\n\n\\end{Code})|(\n{3,})|(_why,)/g, function(all, codeSpace, manyBlanks, why) {
     if (codeSpace)
       return codeSpace.slice(1);
     if (manyBlanks)
       return "\n\n";
+    if (why)
+      return "\_why,";
   }));
 });
 process.stdin.resume();

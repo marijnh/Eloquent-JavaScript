04fc9fac2da056255d178e25209434667ce4d15f
Marijn Haverbeke <marijnh@gmail.com>
2014-11-19 13:54:55 +0100

    Properly orient chessboard in exercise 2.3

--- code/solutions/02_3_chess_board.js
+++ code/solutions/02_3_chess_board.js
@@ -5,9 +5,9 @@ var board = "";
 for (var y = 0; y < size; y++) {
   for (var x = 0; x < size; x++) {
     if ((x + y) % 2 == 0)
-      board += "#";
-    else
       board += " ";
+    else
+      board += "#";
   }
   board += "\n";
 }

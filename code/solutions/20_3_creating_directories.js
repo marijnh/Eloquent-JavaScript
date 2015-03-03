130e8d85d5b91d7ea1d31008b6cc7325a0f629e2
Marijn Haverbeke <marijnh@gmail.com>
2015-03-03 08:06:35 +0100

    Fix solution code for exercise 20.3

--- code/solutions/20_3_creating_directories.js
+++ code/solutions/20_3_creating_directories.js
@@ -1,15 +1,15 @@
 // This code won't work on its own, but is also included in the
 // code/file_server.js file, which defines the whole system.
 
-methods.MKCOL = function(path, request, response) {
+methods.MKCOL = function(path, respond) {
   fs.stat(path, function(error, stats) {
     if (error && error.code == "ENOENT")
-      fs.mkdir(path, respondErrorOrNothing(response));
+      fs.mkdir(path, respondErrorOrNothing(respond));
     else if (error)
-      respond(500, error.toString(), response);
+      respond(500, error.toString());
     else if (stats.isDirectory())
-      respond(204, null, response);
+      respond(204);
     else
-      respond(400, "File exists", response);
+      respond(400, "File exists");
   });
 };

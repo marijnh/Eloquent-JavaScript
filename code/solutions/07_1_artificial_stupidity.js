cdf1ac8ab60381949daf36bb21129b3c54caac83
Tiago Oliveira <tiago.co@gmail.com>
2015-09-06 20:02:44 +0100

    Rename act method parameter in Chapter 7

--- code/solutions/07_1_artificial_stupidity.js
+++ code/solutions/07_1_artificial_stupidity.js
@@ -2,14 +2,14 @@ function SmartPlantEater() {
   this.energy = 30;
   this.direction = "e";
 }
-SmartPlantEater.prototype.act = function(context) {
-  var space = context.find(" ");
+SmartPlantEater.prototype.act = function(view) {
+  var space = view.find(" ");
   if (this.energy > 90 && space)
     return {type: "reproduce", direction: space};
-  var plants = context.findAll("*");
+  var plants = view.findAll("*");
   if (plants.length > 1)
     return {type: "eat", direction: randomElement(plants)};
-  if (context.look(this.direction) != " " && space)
+  if (view.look(this.direction) != " " && space)
     this.direction = space;
   return {type: "move", direction: this.direction};
 };

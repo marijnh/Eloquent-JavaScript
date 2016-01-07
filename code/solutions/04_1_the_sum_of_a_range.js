9ed2ee32f38081e8a457b3088db5249f61ffde13
Marijn Haverbeke <marijnh@gmail.com>
2016-01-07 22:41:16 +0100

    Add additional test in code for exercise 4.1
    Issue #190

--- code/solutions/04_1_the_sum_of_a_range.js
+++ code/solutions/04_1_the_sum_of_a_range.js
@@ -19,7 +19,9 @@ function sum(array) {
   return total;
 }
 
-console.log(sum(range(1, 10)));
-// → 55
+console.log(range(1, 10))
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55

13d4d4b54c1b8afd1fdf7a1231bbcd1a8d668ca7
Libby Horacek <libby@positiondev.com>
2016-01-19 21:52:47 -0500

    Add placeImage

--- html/js/sample.js
+++ html/js/sample.js
@@ -3,3 +3,7 @@ circle(30, "red");
 print("Hello world!");
 circle(50, "black");
 print(1 + 2 - 3);
+
+
+placeImage(circle(20,"black"),circle(10,"white"));
+placeImage(circle(10,"black"),circle(20,"white"));
--- html/js/tlc.js
+++ html/js/tlc.js
@@ -26,7 +26,7 @@ function _addOutput(content) {
   return container;
 }
 
-
+/* print :: anything -> nothing */
 function print(value) {
   var pre = document.createElement("pre");
   pre.textContent = String(value);
@@ -34,18 +34,112 @@ function print(value) {
   _addOutput(pre);
 }
 
+/* circle :: number -> color -> shape */
 function circle(radius, color) {
   var canvas = document.createElement("canvas");
   canvas.width = radius * 2;
   canvas.height = radius * 2;
 
   var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.fillStyle = color;
-  ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
-  ctx.fill();
 
   _addOutput(canvas);
 
-  return { tlc_dt: "circle", radius: radius, color: color };
+  var circ = { tlc_dt: "circle",
+               width: radius * 2,
+               height: radius * 2,
+               radius: radius,
+               color: color };
+  draw(ctx, [circ]);
+
+  return [circ];
 }
+
+/* rectangle :: number -> number -> color -> shape */
+function rectangle(width, height, color) {
+  var canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+
+  var ctx = canvas.getContext("2d");
+
+  _addOutput(canvas);
+  
+  var rect = { tlc_dt: "rectangle",
+               width: width,
+               height: height,
+               color: color };
+
+  draw(ctx, [rect]);
+
+  return [rect];
+}
+
+/* draw :: context -> shape -> nothing */
+function draw(ctx, shapes) {
+  
+  for (var i = 0; i < shapes.length; i++) {
+    var shape = shapes[i];
+    switch (shape.tlc_dt) {
+    case "rectangle":
+      ctx.fillStyle = shape.color;
+      ctx.fillRect(0, 0, shape.width, shape.height);
+      break;
+    case "circle":
+      ctx.beginPath();
+      ctx.fillStyle = shape.color;
+      ctx.arc(shape.radius,
+              shape.radius,
+              shape.radius, 0, 2 * Math.PI);
+      ctx.fill();
+      break;
+    default:
+      break;
+    }
+  }
+}
+  
+/* placeImage :: shape -> shape -> shape */
+function placeImage(shape1, shape2, x, y) {
+  var canvas = document.createElement("canvas");
+  var shapes = shape1.concat(shape2);
+  canvas.width = largestX(shapes);
+  canvas.height = largestY(shapes);
+
+  var ctx = canvas.getContext("2d");
+
+  draw(ctx, shapes);
+  
+  _addOutput(canvas);
+
+  return shapes;
+}
+
+function largestX (shapes) {
+  var max = -Infinity;
+  
+  for (var i = 0; i < shapes.length; i++) {
+
+    if (shapes[i].width > max) {
+      max = shapes[i].width;
+    }
+  }
+
+  return max;
+}
+
+function largestY (shapes) {
+  var max = -Infinity;
+  
+  for (var i = 0; i < shapes.length; i++) {
+
+    if (shapes[i].height > max) {
+      max = shapes[i].height;
+    }
+  }
+
+  return max;
+}
+    
+  
+  
+  

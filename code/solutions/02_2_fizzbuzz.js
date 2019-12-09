9d51e9b5b00087c7b46b23a0eefd86df08ed80f1
chak-eeb <55099578+chak-eeb@users.noreply.github.com>
2019-12-08 21:09:28 -0500

    add fizzBuzz
    added the 3rd option where num or n is divisible by 3 and by 5.

--- code/solutions/02_2_fizzbuzz.js
+++ code/solutions/02_2_fizzbuzz.js
@@ -4,3 +4,15 @@ for (let n = 1; n <= 100; n++) {
   if (n % 5 == 0) output += "Buzz";
   console.log(output || n);
 }
+
+for(let num = 1; num<=100; num++){
+  if(num % 3 == 0 && num % 5 == 0){
+    console.log("fizzBuzz");
+  } else if(num % 5== 0){
+    console.log("buzz");
+  } else if (num % 3 == 0){
+    console.log("fizz");
+  } else{
+    console.log(num);
+  }
+}

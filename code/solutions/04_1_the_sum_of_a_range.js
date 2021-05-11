fc62c1d73293b1633a2acd38bd19aa8d59fb047d
nilthedev <nilthedev@gmail.com>
2021-05-12 00:54:03 +0530

    fixed infinite loop bug

--- code/solutions/04_1_the_sum_of_a_range.js
+++ code/solutions/04_1_the_sum_of_a_range.js
@@ -3,7 +3,7 @@ function range(start, end, step = start < end ? 1 : -1) {
 
   if (step > 0) {
     for (let i = start; i <= end; i += step) array.push(i);
-  } else {
+  } else if (step < 0) {
     for (let i = start; i >= end; i += step) array.push(i);
   }
   return array;
@@ -23,3 +23,5 @@ console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
+console.log(range(20, 10, 0))
+// → []
